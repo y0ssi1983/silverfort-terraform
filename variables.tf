6ca2b15eca2ad0b899c279d52bc4171581f5891c
y0ssi1983 <50336741+y0ssi1983@users.noreply.github.com>
2022-07-04 12:56:33 +0300

    Update variables.tf
    rename for environment issue

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name_prefix" {
-  default       = "interview6-rg"
+  default       = "rg"
   description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
